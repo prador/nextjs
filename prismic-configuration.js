ea514f23464e5259f54226190936818bc94aeffb
PradyumnaSurampudi <71778248+PradyumnaSurampudi@users.noreply.github.com>
2020-11-05 01:37:49 +0100

    no idea

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -28,7 +28,7 @@
 import Prismic from "prismic-javascript"
 
 // Prismic API endpoint
-export const apiEndpoint = 'https://nextest3.cdn.prismic.io/api/v2'
+export const apiEndpoint = 'https://nextest3.prismic.io/api'
 
 // Access Token if the repository is not public
 // Generate a token in your dashboard and configure it here if your repository is private
